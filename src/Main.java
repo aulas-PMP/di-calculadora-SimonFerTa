5888c6bd34f77772fa2bbf9a6cae42483b18d272
Simon <simonf.tacon@gmail.com>
2024-12-10 17:53:05 +0100

    Selección de modos implementado.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Calculadora ccd = new Calculadora(MODE.FREE_MODE);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(
+            "Elige el modo para abrir la calculadora:\n" +
+            "1.- Sólo ratón.\n" +
+            "2.- Sólo teclado\n" +
+            "3.- Modo libre (ambos)\n" +
+            "Opción: "
+        );
+        int opcion = sc.nextInt()-1;
+        while (opcion < 0 || opcion > 2) {
+            System.out.print("Opción: ");
+            opcion = sc.nextInt()-1;
+        }
+        sc.close();
+
+        Calculadora ccd = new Calculadora(opcion);
         ccd.setVisible(true);
     }
 }
